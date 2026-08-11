ca7ec5ff035d971e481bda78a82f5cfd6bf892e2
Martin Atkins <mart@degeneration.co.uk>
2026-08-10 17:04:39 -0700

    jsonfunction: Special trickery for the "convert" function
    The recently-added "convert" function has an argument that takes a type
    rather than a value. That means tools like the language server will need to
    know to treat that argument differently for code completion, etc.
    
    To handle that we introduce a new JSON type serialization "type" to
    represent this special situation. Since this is not a type that cty itself
    knows how to serialize, we handle it as a special case.
    
    We also need to provide a fixed conversion to the json FunctionSignature
    type because the normal one works by passing DynamicPseudoType for all of
    the declared parameters, and that isn't a valid way to call convert. The
    custom function just specifies lang.TypeConversionType directly so we don't
    need to try to sniff that out from the function's "Type" method.
    
    Signed-off-by: Martin Atkins <mart@degeneration.co.uk>

--- internal/command/views/jsonfunction/parameter.go
+++ internal/command/views/jsonfunction/parameter.go
@@ -6,7 +6,8 @@
 package jsonfunction
 
 import (
-	"github.com/zclconf/go-cty/cty"
+	"encoding/json"
+
 	"github.com/zclconf/go-cty/cty/function"
 )
 
@@ -23,7 +24,7 @@ type parameter struct {
 	IsNullable bool `json:"is_nullable,omitempty"`
 
 	// A type that any argument for this parameter must conform to.
-	Type cty.Type `json:"type"`
+	Type json.RawMessage `json:"type"`
 }
 
 func marshalParameter(p *function.Parameter) *parameter {
@@ -35,7 +36,7 @@ func marshalParameter(p *function.Parameter) *parameter {
 		Name:        p.Name,
 		Description: p.Description,
 		IsNullable:  p.AllowNull,
-		Type:        p.Type,
+		Type:        marshalType(p.Type),
 	}
 }
 
